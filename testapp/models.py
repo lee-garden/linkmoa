64096fffbd910127d2d03c5dde831a1c3a6fb4ee
garden <khjvvv7@gmail.com>
2019-07-05 18:29:39 +0900

    edit Memo class
    Add directory, display field
    Import jsonfield

--- testapp/models.py
+++ testapp/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+import jsonfield
 
 # Create your models here.
 class Memo(models.Model):
-    user_id = models.IntegerField(max_length=20)
+    user_id = models.IntegerField()
+    directory = models.CharField(max_length=20, default="recently")
+    display = models.CharField(max_length=10, default="visible")
     keyword = models.CharField(max_length=30)
     urls = models.TextField(default=None)
     memo = models.TextField(default="")
@@ -10,3 +13,6 @@ class Memo(models.Model):
     def __str__(self):
         return self.keyword
     
+    def split(urls):
+        urlList = urls.split('\n')
+        return urlList

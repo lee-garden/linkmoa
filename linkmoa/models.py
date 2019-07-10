e2d23258d0d3957d736c5ad8f6808b8454b8fc00
garden <khjvvv7@gmail.com>
2019-07-10 11:05:20 +0900

    Extend user model
    Extend user model using OnetoOne model user - profile
    Each user has 10 directories

--- linkmoa/models.py
+++ linkmoa/models.py
@@ -1,5 +1,8 @@
-from django.db import models
-from django.contrib.auth.models import User
+from django.db import models  
+from django.contrib.auth.models import User  
+from django.db.models.signals import post_save  
+from django.dispatch import receiver
+
 
 # Create your models here.
 class Memo(models.Model):
@@ -16,3 +19,26 @@ class Memo(models.Model):
     def split(urls):
         urlList = urls.split('\n')
         return urlList
+
+class Profile(models.Model):  
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    dir1 = models.CharField(max_length=30, blank=True)
+    dir2 = models.CharField(max_length=30, blank=True)
+    dir3 = models.CharField(max_length=30, blank=True)
+    dir4 = models.CharField(max_length=30, blank=True)
+    dir5 = models.CharField(max_length=30, blank=True)
+    dir6 = models.CharField(max_length=30, blank=True)
+    dir7 = models.CharField(max_length=30, blank=True)
+    dir8 = models.CharField(max_length=30, blank=True)
+    dir9 = models.CharField(max_length=30, blank=True)
+    dir10 = models.CharField(max_length=30, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):  
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):  
+    instance.profile.save()
